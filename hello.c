97d9921023523b7356de3fb088ea47b12393b412
admin <admin@localhost.localdomain>
2017-06-20 18:29:48 -0700

    i don't know

--- hello.c
+++ hello.c
@@ -1,6 +1,68 @@
 #include <stdio.h>
 
-int main() { printf("hello world\n"); return 0;}
+int main() {
+ //int val = 35;
+ //printf("hello world\n value = %d. \n new value:", val);
+ //scanf("%d", &val);
+ //printf(" new value: %d. \n", val);
+
+ //int test = 10;
+ //char c;
+
+ //printf("c is now: %d. \n", c);
+ printf("\n\n int:%d\nmod:%d\nfloat:%f\n",4/3,4%3,4.0/3 );
+ // this is an example of false = 0 and anything else = 1
+ printf(" false = %d \n true= %d\n", (3==2),(3==3) );
+/*
+ //whats true and whats false...nobody knows
+ printf("3>2\n");
+ if(3>2) {
+   printf("true\n");
+ } else {
+   printf("false\n");
+ }
+ printf("3<2\n");
+ if(3<2) {
+   printf("true\n");
+ } else {
+   printf("false\n");
+ }
+ printf("3-(2+1)\n");
+ if(3-(2+1) ) {
+   printf("true\n");
+ } else {
+   printf("false\n");
+ }
+ printf("d\n");
+ if('d') {
+   printf("true\n");
+ } else {
+   printf("false\n");
+ }
+ printf("9%%3\n");
+ if(9%3) {
+   printf("true\n");
+ } else {
+   printf("false\n");
+ }
+ printf("3>2\n");
+ if((3>2)-1) {
+   printf("true\n");
+ } else {
+   printf("false\n");
+ }
+*/
+// switches
+
+
+
+
+
+ return 0;
+
+
+
+
+}
 
 // no way kemosabe
-
